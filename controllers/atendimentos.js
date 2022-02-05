541aa08b57dd5cf9af83456deac36919137ad853
edemuner <eduardood15@hotmail.com>
2022-02-05 18:09:53 -0300

    created a model to manage queries to add atendimentos, that model was called in controllers to add the data from POST requests into the database

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,13 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentose está realizando um get'))
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um get'))
 
     app.post('/atendimentos', (req, res) => {
 
-        console.log(req.body)
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
         res.send('Você está na rota de atendimentos e está realizando um POST')
     
     })
