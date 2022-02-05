21f13de032d38d0da1ea84bb01d85be208c95e3f
edemuner <eduardood15@hotmail.com>
2022-02-05 15:03:56 -0300

    adicionado um metodo POST na rota atendimentos (testado no postman)

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,5 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentose está realizando um get'))
+
+    app.post('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um POST,'))
 }
