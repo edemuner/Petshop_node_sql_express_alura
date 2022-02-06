2134860df3fd8b573daf5c70a8ee77b66667fe23
edemuner <eduardood15@hotmail.com>
2022-02-06 15:46:55 -0300

    created a Servicos table, and created a method inside Tabelas, which is also called at init, that makes the query if table doesn't exist

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -4,16 +4,28 @@ class Tabelas {
         this.conexao = conexao
         console.log('Tabelas foram chamadas')
         this.criarAtendimentos()
+        this.criarServicos()
     }
 
     criarAtendimentos(){
 
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id), idserv int, FOREIGN KEY (idserv) REFERENCES Atendimentos(id))'
 
         this.conexao.query(sql, (erro) => {
             if (erro) console.log(erro)
             else {
                 console.log('Tabela de atendimentos criada com sucesso')
+            }
+        })
+    }
+
+    criarServicos(){
+        const sql = 'CREATE TABLE IF NOT EXISTS Servicos (id int NOT NULL AUTO_INCREMENT, nome varchar(15) NOT NULL, preco float NOT NULL, PRIMARY KEY (id))'
+    
+        this.conexao.query(sql, (erro) => {
+            if (erro) console.log(erro)
+            else {
+                console.log('Criada tabela de serviços')
             }
         })
     }
