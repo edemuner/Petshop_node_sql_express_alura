4e2e9430291c5f93e3ef0537c6aef989851a6b41
edemuner <eduardood15@hotmail.com>
2022-02-06 11:34:27 -0300

    inserted date validation to accept only future dates and client names >= 5 chars

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -8,6 +8,35 @@ class Atendimento {
 
         const data = moment(atendimento.data, 'DD-MM-YYYY hh:mm:ss').format('YYYY-MM-DD HH:mm:ss')
 
+        // data válida somente se no futuro
+        const dataValida = moment(data).isSameOrAfter(dataCriacao)
+
+        const clienteValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido:dataValida,
+                mensagem: 'Data dve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ]
+
+        // filtro do objeto acima para encontrar campos inválidos
+        const erros =  validacoes.filter(campo => !campo.valido)
+
+        const existemErros = erros.length
+
+        // se esse filtro tiver qualquer comprimento, a query não é executada e o erro é exibido
+        if (existemErros){
+            res.status(400).json(erros)
+        }
+        else {
+
         const atendimentoDatado = {...atendimento, dataCriacao, data}
 
         const sql = 'INSERT INTO Atendimentos SET ?'
@@ -20,6 +49,9 @@ class Atendimento {
                 res.status(201).json(resultados)
             }
         })
+        }
+        
+        
     }
 }
 
