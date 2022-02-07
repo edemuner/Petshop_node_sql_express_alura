b8da7c93c412330d904e53c44086a2431d3ae8c2
edemuner <eduardood15@hotmail.com>
2022-02-07 16:03:46 -0300

    the relationship between pets model and uploadDeArquivos was adjusted to only do the query if the file extension is right

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -5,16 +5,18 @@ module.exports = (caminho, nomeDoArquivo, callBackImagemCriada) => {
 
     const tiposValidos = ['.jpg', '.png', '.jpeg']
     const tipo = path.extname(caminho)
-    const tipoEValido = tiposValidos.indexOf(tipo)
+    const tipoInvalido = tiposValidos.indexOf(tipo) === -1
 
-    if (tipoEValido === -1){
+    if (tipoInvalido){
+        const erro = 'Tipo é inválido'
         console.log('Erro, extensão do arquivo inválida')
+        callBackImagemCriada(erro)
     } else {
         const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
 
         fs.createReadStream(caminho)
         .pipe(fs.createWriteStream(novoCaminho))
-        .on('finish', () => callBackImagemCriada(novoCaminho))
+        .on('finish', () => callBackImagemCriada(false, novoCaminho))
     }
  
 }
--- models/pets.js
+++ models/pets.js
@@ -5,23 +5,26 @@ class Pet {
 
     adiciona(pet, res){
 
-        const sql = 'INSERT INTO Pets SET ?'
 
-        uploadDeArquivo(pet.imagem, pet.nome, (novoCaminho) => {
-            const novoPet = {
-                nome:pet.nome,
-                imagem:novoCaminho
+        uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+            if (erro){
+                res.status(400).json({erro})
+            } else {
+                const novoPet = {
+                    nome:pet.nome,
+                    imagem:novoCaminho
+                }
+                const sql = 'INSERT INTO Pets SET ?'
+                conexao.query(sql, novoPet, erro => {
+                    if (erro) {
+                        console.log(erro)
+                        res.status(400).json(erro)
+                    } else {
+                        res.status(200).json(novoPet)
+                    }            
+                })
             }
-            conexao.query(sql, novoPet, erro => {
-                if (erro) {
-                    console.log(erro)
-                    res.status(400).json(erro)
-                } else {
-                    res.status(200).json(novoPet)
-                }            
-            })
         })
-        
     }
 }
 
