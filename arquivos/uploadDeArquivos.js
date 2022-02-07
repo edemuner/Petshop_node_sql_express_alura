c63641d62be961d2569d6b2565685298c09dd34c
edemuner <eduardood15@hotmail.com>
2022-02-07 10:18:59 -0300

    a few more tests, using asynchronous functions, createReadStream and createWriteStream

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,10 +1,6 @@
 const fs = require('fs')
 
-fs.readFile('./assets/salsicha.jpg', (erro, buffer) => {
-    console.log('imagem foi bufferizada')
-
-    fs.writeFile('./assets/salsicha2.jpg', buffer, (erro) => {
-        console.log('imagem foi escrita')
-    })
-})
+fs.createReadStream('./assets/salsicha.jpg')
+    .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg'))
+    .on('finish', () => console.log('imagem escrita com sucesso'))
 
