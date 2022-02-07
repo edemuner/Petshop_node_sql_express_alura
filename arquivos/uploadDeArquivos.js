47fde743ce8dc77d84f531f8cc359331eb8c7a70
edemuner <eduardood15@hotmail.com>
2022-02-07 15:20:18 -0300

    a check for the extension of the uploaded file was added in uploadDeArquivos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,11 +1,21 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = (caminho, nomeDoArquivo, callBackImagemCriada) => {
 
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
-    
-    fs.createReadStream(caminho)
-    .pipe(fs.createWriteStream(novoCaminho))
-    .on('finish', () => callBackImagemCriada(novoCaminho))
+    const tiposValidos = ['.jpg', '.png', '.jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEValido = tiposValidos.indexOf(tipo)
+
+    if (tipoEValido === -1){
+        console.log('Erro, extensão do arquivo inválida')
+    } else {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+
+        fs.createReadStream(caminho)
+        .pipe(fs.createWriteStream(novoCaminho))
+        .on('finish', () => callBackImagemCriada(novoCaminho))
+    }
+ 
 }
 
