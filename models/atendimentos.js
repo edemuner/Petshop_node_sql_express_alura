bca0bd5613338204364c50aa9b36dfb23db66278
edemuner <eduardood15@hotmail.com>
2022-02-09 10:17:34 -0300

    the validations of client and date were encapsulated in the constructor of Atendimento, which reads the parameters passed by the function inside adicionar

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,32 +5,45 @@ const repositorio = require('../repositorios/atendimentos')
 
 class Atendimento {
 
+    constructor(){
+        // data válida somente se no futuro
+        this.dataValida = (data, dataCriacao) => moment(data).isSameOrAfter(dataCriacao)
+
+        this.clienteValido = (tamanho) => tamanho >= 5
+
+        this.valida = parametros => this.validacoes.filter(campo => {
+             const {nome} = campo
+             const parametro = parametros[nome]
+
+             return !campo.valido(parametro)
+        })
+
+        this.validacoes = [
+            {
+                nome: 'data',
+                valido:this.dataValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: this.clienteValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ]
+    }
+
     adiciona(atendimento){
         
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
 
         const data = moment(atendimento.data, 'DD-MM-YYYY hh:mm:ss').format('YYYY-MM-DD HH:mm:ss')
 
-        // data válida somente se no futuro
-        const dataValida = moment(data).isSameOrAfter(dataCriacao)
+        const parametros = {
+            data: { data, dataCriacao },
+            cliente: { tamanho: atendimento.cliente.length }
+        }
 
-        const clienteValido = atendimento.cliente.length >= 5
-
-        const validacoes = [
-            {
-                nome: 'data',
-                valido:dataValida,
-                mensagem: 'Data deve ser maior ou igual a data atual'
-            },
-            {
-                nome: 'cliente',
-                valido: clienteValido,
-                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
-            }
-        ]
-
-        // filtro do objeto acima para encontrar campos inválidos
-        const erros =  validacoes.filter(campo => !campo.valido)
+        const erros =  this.valida(parametros)
 
         const existemErros = erros.length
 
